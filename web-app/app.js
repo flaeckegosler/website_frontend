70e8d84bf4e22af4b321d90ab544539da5303e64
reffx <ripsli@hotmail.com>
2020-09-02 14:55:40 +0200

    basics news

--- web-app/app.js
+++ web-app/app.js
@@ -4,9 +4,22 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(__dirname + '/../web-app'));
+app.use(express.static(__dirname + '/ajax.googleapis.com'));
+app.use(express.static(__dirname + '/cdnjs.cloudflare.com'));
+app.use(express.static(__dirname + '/code.jquery.com'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/use.fontawesome.com'));
+
+
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
+app.get('/test', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/main.html'));
+});
+
 
 
 app.listen(3000, function(){
