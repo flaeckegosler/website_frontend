8949fdcdb9e56d3d0573204d290628bd6f8516ba
DESKTOP-QIONQIP\ripsl <ripsli@hotmail.com>
2021-11-05 13:44:12 +0100

    #31: Fadeout improved

--- lib/sections/news/components/news_card.dart
+++ lib/sections/news/components/news_card.dart
@@ -6,22 +6,29 @@ import 'package:website_frontend/provider/news_provider.dart';
 import 'package:flutter_html/flutter_html.dart';
 
 class NewsCard extends StatefulWidget {
-  const NewsCard({
-    required this.index,
-  });
-
+  final key;
   final int index;
 
+  const NewsCard({required this.index, required this.key});
+
   @override
-  _NewsCardState createState() => _NewsCardState();
+  NewsCardState createState() => NewsCardState();
 }
 
-class _NewsCardState extends State<NewsCard> {
+class NewsCardState extends State<NewsCard> with TickerProviderStateMixin {
   bool _isLoading = false;
+
+  late AnimationController fadeController;
+  late Animation _fadeInAnimation;
 
   @override
   initState() {
     fetchNewsList();
+    fadeController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _fadeInAnimation = Tween(begin: 1.0, end: 0.0).animate(fadeController);
     super.initState();
   }
 
@@ -63,61 +70,65 @@ class _NewsCardState extends State<NewsCard> {
                 borderRadius: BorderRadius.circular(10),
                 boxShadow: [if (isHover) kDefaultCardShadow],
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    height: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              _newsProvider.allNews[widget.index].imageURL),
-                          fit: BoxFit.cover),
+              child: FadeTransition(
+                opacity: _fadeInAnimation as Animation<double>,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                _newsProvider.allNews[widget.index].imageURL),
+                            fit: BoxFit.cover),
+                      ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const Icon(Icons.access_time, size: 14),
-                      Text(
-                          " ${_newsProvider.allNews[widget.index].timeCreatedFormatted}",
-                          style: const TextStyle(fontSize: 12)),
-                    ],
-                  ),
-                  Column(
-                    //This column is used to reduce space between title and intro text
-                    children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Container(
-                          padding: const EdgeInsets.only(left: 18, right: 18),
-                          child: Text(
-                            _newsProvider.allNews[widget.index].newsTitle,
-                            style: const TextStyle(
-                                fontWeight: FontWeight.w800, fontSize: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Icon(Icons.access_time, size: 14),
+                        Text(
+                            " ${_newsProvider.allNews[widget.index].timeCreatedFormatted}",
+                            style: const TextStyle(fontSize: 12)),
+                      ],
+                    ),
+                    Column(
+                      //This column is used to reduce space between title and intro text
+                      children: [
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Container(
+                            padding: const EdgeInsets.only(left: 18, right: 18),
+                            child: Text(
+                              _newsProvider.allNews[widget.index].newsTitle,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w800, fontSize: 16),
+                            ),
                           ),
                         ),
-                      ),
-                      Html(
-                        data: _newsProvider.allNews[widget.index].newsIntroText,
-                        style: {
-                          "p": Style(
-                            fontFamily: 'serif',
-                            padding: const EdgeInsets.only(
-                              left: 10,
-                              right: 10,
+                        Html(
+                          data:
+                              _newsProvider.allNews[widget.index].newsIntroText,
+                          style: {
+                            "p": Style(
+                              fontFamily: 'serif',
+                              padding: const EdgeInsets.only(
+                                left: 10,
+                                right: 10,
+                              ),
+                              fontSize: FontSize.em(1),
                             ),
-                            fontSize: FontSize.em(1),
-                          ),
-                        },
-                      ),
-                    ],
-                  ),
-                  buildRedakteur(
-                      _newsProvider.allNews[widget.index].newsCreatedBy),
-                  const SizedBox(height: 20),
-                ],
+                          },
+                        ),
+                      ],
+                    ),
+                    buildRedakteur(
+                        _newsProvider.allNews[widget.index].newsCreatedBy),
+                    const SizedBox(height: 20),
+                  ],
+                ),
               ),
             ),
           ),
