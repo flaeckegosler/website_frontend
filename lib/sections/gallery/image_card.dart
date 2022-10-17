594a3d57c939b9ffbdabac11f882691e792ca0c6
DESKTOP-QIONQIP\ripsl <ripsli@hotmail.com>
2022-10-17 19:43:21 +0200

    #35: Zoom images

--- lib/sections/gallery/image_card.dart
+++ lib/sections/gallery/image_card.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 import 'package:website_frontend/models/pictures.dart';
 
 class ImageCard extends StatelessWidget {
@@ -9,10 +10,14 @@ class ImageCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(16.0),
-      child:
-          CachedNetworkImage(imageUrl: imageData.pictureUrl, fit: BoxFit.cover),
+    return InkWell(
+      onTap: () => context.vxNav
+          .push(Uri(path: '/specific_image'), params: imageData.pictureUrl),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16.0),
+        child: CachedNetworkImage(
+            imageUrl: imageData.pictureUrl, fit: BoxFit.cover),
+      ),
     );
   }
 }
---
+++ lib/sections/member/simple_photo_view_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+
+class SimplePhotoViewPage extends StatefulWidget {
+  final String galleryLink;
+
+  const SimplePhotoViewPage(this.galleryLink);
+
+  @override
+  _SimplePhotoViewPageState createState() => _SimplePhotoViewPageState();
+}
+
+class _SimplePhotoViewPageState extends State<SimplePhotoViewPage> {
+  late PhotoViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = PhotoViewController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(147, 90, 161, 1),
+      ),
+      body: PhotoView(
+        imageProvider: NetworkImage(
+          widget.galleryLink,
+        ),
+        // Contained = the smallest possible size to fit one dimension of the screen
+        minScale: PhotoViewComputedScale.contained * 0.8,
+        // Covered = the smallest possible size to fit the whole screen
+        maxScale: PhotoViewComputedScale.covered * 2,
+        enableRotation: true,
+        // Set the background color to the "classic white"
+        backgroundDecoration: BoxDecoration(
+          color: Theme.of(context).canvasColor,
+        ),
+      ),
+    );
+  }
+}
