dfba6e5ca97f349d6aa51a02069ae418086b6981
DESKTOP-QIONQIP\ripsl <ripsli@hotmail.com>
2022-10-18 21:21:09 +0200

    #105: gallery improvement

--- lib/sections/member/simple_photo_view_page.dart
+++ lib/sections/member/simple_photo_view_page.dart
@@ -28,33 +28,27 @@ class _SimplePhotoViewPageState extends State<SimplePhotoViewPage> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        context.vxNav.pop();
-        return true;
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(147, 90, 161, 1),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(147, 90, 161, 1),
+      ),
+      body: PhotoView(
+        imageProvider: NetworkImage(
+          widget.galleryLink,
         ),
-        body: PhotoView(
-          imageProvider: NetworkImage(
-            widget.galleryLink,
+        loadingBuilder: (context, event) => const Center(
+          child: CircularProgressIndicator(
+            color: Color.fromRGBO(147, 90, 161, 1),
           ),
-          loadingBuilder: (context, event) => const Center(
-            child: CircularProgressIndicator(
-              color: Color.fromRGBO(147, 90, 161, 1),
-            ),
-          ),
-          // Contained = the smallest possible size to fit one dimension of the screen
-          minScale: PhotoViewComputedScale.contained * 0.8,
-          // Covered = the smallest possible size to fit the whole screen
-          maxScale: PhotoViewComputedScale.covered * 2,
-          enableRotation: true,
-          // Set the background color to the "classic white"
-          backgroundDecoration: BoxDecoration(
-            color: Theme.of(context).canvasColor,
-          ),
+        ),
+        // Contained = the smallest possible size to fit one dimension of the screen
+        minScale: PhotoViewComputedScale.contained * 0.8,
+        // Covered = the smallest possible size to fit the whole screen
+        maxScale: PhotoViewComputedScale.covered * 2,
+        enableRotation: true,
+        // Set the background color to the "classic white"
+        backgroundDecoration: BoxDecoration(
+          color: Theme.of(context).canvasColor,
         ),
       ),
     );
