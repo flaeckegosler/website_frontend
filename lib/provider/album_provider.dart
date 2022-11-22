10292488144300311a14448525114d23f9437f58
DESKTOP-QIONQIP\ripsl <ripsli@hotmail.com>
2022-11-22 07:39:45 +0100

    #134: swipe through gallery

---
+++ lib/provider/album_provider.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:website_frontend/models/album.dart';
+
+class PicturesProvider with ChangeNotifier {
+  List<Album> _albums = [];
+
+  List<Album> get allPictures {
+    return List.from(_albums);
+  }
+
+  int getAmountOfPictures(String albumTitle) {
+    return int.parse(_albums
+        .where((element) => element.albumTitle == albumTitle)
+        .first
+        .specificImage
+        .last
+        .pictureIndex);
+  }
+
+  SpecificImage getGalleryLink(albumTitle, pictureIndex) {
+    Album selectedGalerie =
+        _albums.firstWhere((element) => (element.albumTitle == albumTitle));
+    return selectedGalerie.specificImage.firstWhere((element) =>
+        element.pictureIndex.toString() == pictureIndex.toString());
+  }
+
+  Future<void> fetchAlbumList() async {
+    final url = Uri.parse('https://flaeckegosler.ch/app/pics-to-json/');
+    final response = await http.get(
+      url,
+    );
+    int pictureIndex = 1;
+    final List<Album> fetchedPicturesList = [];
+    final Map<String, dynamic> picturesListData =
+        json.decode(response.body) as Map<String, dynamic>;
+
+    picturesListData.forEach((
+      String picturesId,
+      dynamic picturesData,
+    ) {
+      //Resolve map to List
+      List<SpecificImage> temp = [];
+      picturesData['pictures'].forEach(
+        (k, v) => {
+          temp.add(
+            SpecificImage(
+              pictureName: k as String,
+              pictureUrl: v as String,
+              pictureIndex: pictureIndex.toString(),
+            ),
+          ),
+          pictureIndex = pictureIndex + 1
+        },
+      );
+      pictureIndex = 1;
+      final Album pictures = Album(
+          id: picturesId.toString(),
+          menuTitle: picturesData['menuTitle'] as String,
+          albumTitle: picturesData['albumTitle'] as String,
+          bodyText: picturesData['bodyText'] as String,
+          dateUnix: picturesData['dateUnix'] as int,
+          dateFormatted: picturesData['dateFormatted'] as String,
+          specificImage: temp);
+      fetchedPicturesList.add(pictures);
+    });
+    _albums = fetchedPicturesList;
+  }
+}
---
+++ lib/sections/gallery/galerie_photo_view_page.dart
@@ -0,0 +1,165 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:provider/provider.dart';
+import 'package:website_frontend/models/album.dart';
+import 'package:website_frontend/provider/album_provider.dart';
+
+class GaleriePhotoViewPage extends StatefulWidget {
+  final String albumTitle;
+  final String pictureIndex;
+
+  const GaleriePhotoViewPage(this.albumTitle, this.pictureIndex);
+
+  @override
+  _GaleriePhotoViewPageState createState() => _GaleriePhotoViewPageState();
+}
+
+class _GaleriePhotoViewPageState extends State<GaleriePhotoViewPage> {
+  late PhotoViewController controller;
+  late SpecificImage specificImage;
+  bool _isLoading = false;
+  late int startIndex;
+  late int maxIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _isLoading = true;
+    });
+    startIndex = int.parse(this.widget.pictureIndex);
+    if (Provider.of<PicturesProvider>(context, listen: false)
+        .allPictures
+        .isEmpty) {
+      Provider.of<PicturesProvider>(context, listen: false)
+          .fetchAlbumList()
+          .then((value) => specificImage =
+              Provider.of<PicturesProvider>(context, listen: false)
+                  .getGalleryLink(
+                      this.widget.albumTitle, this.widget.pictureIndex))
+          .then((value) => setState(() {
+                maxIndex = Provider.of<PicturesProvider>(context, listen: false)
+                    .getAmountOfPictures(widget.albumTitle);
+                _isLoading = false;
+              }));
+    } else {
+      specificImage = Provider.of<PicturesProvider>(context, listen: false)
+          .getGalleryLink(this.widget.albumTitle, this.widget.pictureIndex);
+      maxIndex = Provider.of<PicturesProvider>(context, listen: false)
+          .getAmountOfPictures(widget.albumTitle);
+      setState(() {
+        _isLoading = false;
+      });
+    }
+    controller = PhotoViewController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  Widget buildButtons({bool stretch = false}) => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                primary: startIndex < 2
+                    ? Colors.grey
+                    : const Color.fromRGBO(147, 90, 162, 1),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 28, vertical: 15)),
+            onPressed: previous,
+            child: const Icon(
+              Icons.arrow_back,
+              size: 32,
+            ),
+          ),
+          if (stretch)
+            const Spacer()
+          else
+            const SizedBox(
+              width: 32,
+            ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                primary: startIndex > (maxIndex - 1)
+                    ? Colors.grey
+                    : const Color.fromRGBO(147, 90, 162, 1),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 28, vertical: 15)),
+            onPressed: next,
+            child: const Icon(
+              Icons.arrow_forward,
+              size: 32,
+            ),
+          ),
+        ],
+      );
+
+  void previous() {
+    print(maxIndex);
+
+    if (startIndex > 2) {
+      setState(() {
+        context.push('/galerie/${this.widget.albumTitle}/${startIndex - 1}');
+      });
+    }
+  }
+
+  void next() {
+    setState(() {
+      print(maxIndex);
+      if (startIndex < maxIndex) {
+        context.push('/galerie/${this.widget.albumTitle}/${startIndex + 1}');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(147, 90, 161, 1),
+      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+                color: Color.fromRGBO(147, 90, 161, 1),
+              ),
+            )
+          : Column(
+              children: [
+                Expanded(
+                  child: PhotoView(
+                    imageProvider: CachedNetworkImageProvider(
+                      specificImage.pictureUrl,
+                    ),
+                    loadingBuilder: (context, event) => const Center(
+                      child: CircularProgressIndicator(
+                        color: Color.fromRGBO(147, 90, 161, 1),
+                      ),
+                    ),
+                    // Contained = the smallest possible size to fit one dimension of the screen
+                    minScale: PhotoViewComputedScale.contained * 0.8,
+                    // Covered = the smallest possible size to fit the whole screen
+                    maxScale: PhotoViewComputedScale.covered * 2,
+                    enableRotation: true,
+                    // Set the background color to the "classic white"
+                    backgroundDecoration: BoxDecoration(
+                      color: Theme.of(context).canvasColor,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: buildButtons(),
+                )
+              ],
+            ),
+    );
+  }
+}
